7ef571e931bbd85740272c0211c19d495a5d239c
Jack Tran <jacktran14122004@gmail.com>
2025-03-08 23:06:02 -0500

    Update BinaryToStr.py

--- BinaryToStr.py
+++ BinaryToStr.py
@@ -1,6 +1,6 @@
 def binary_to_string(binary_str):
     return ''.join(chr(int(b, 2)) for b in binary_str.split())
 
-# Example usage
+# An example usage
 binary_input = "01001000 01100101 01101100 01101100 01101111"
-print(binary_to_string(binary_input))  # Output: Hello
+print(binary_to_string(binary_input))  # This should output: Hello
